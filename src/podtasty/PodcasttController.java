61e7f55189c9ce8c48342f58bfcb3cb9db28f2ff
OmarBnr1 <amrouchbnr@gmail.com>
2021-04-14 17:37:01 +0100

    podcast <3

--- src/podtasty/PodcasttController.java
+++ src/podtasty/PodcasttController.java
@@ -5,14 +5,23 @@
  */
 package podtasty;
 
+import entities.Podcast;
+import java.awt.Image;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javax.sound.sampled.AudioFileFormat;
+import javax.swing.JFileChooser;
+import services.CRUDPodcast;
 
 /**
  * FXML Controller class
@@ -42,14 +51,40 @@ public class PodcasttController implements Initializable {
 
     @FXML
     private void btnuploadaudio(ActionEvent event) {
+        
+        JFileChooser fchooser = new JFileChooser();
+        fchooser.showOpenDialog(null);
+        File selectedFile = fchooser.getSelectedFile();
+        String filename = selectedFile.getAbsolutePath();
+        AudioFileFormat getAbsolutepath = null ;
+        
     }
 
     @FXML
     private void btnuploadimg(ActionEvent event) {
+        
+        JFileChooser chooser = new JFileChooser();
+        chooser.showOpenDialog(null);
+        File selectedFile = chooser.getSelectedFile();
+        String filename = selectedFile.getAbsolutePath();
+        Image getAbsolutepath = null ;
+       // AudioFileFormat getAbsolutpath = null;
+        //ImageIcon icon = new Image
+        //System.out.println("file name");
     }
 
     @FXML
     private void btnajoutpodcast(ActionEvent event) {
+        
+        Podcast pod = new Podcast (podname.getText(),poddesc.getText());
+        CRUDPodcast crpod = CRUDPodcast.getInstance();
+        crpod.addPodcast(pod);
+        
+        Alert alert = new Alert (AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(null);
+        alert.setContentText("podcast successfully added");
+        alert.show();
     }
     
 }
